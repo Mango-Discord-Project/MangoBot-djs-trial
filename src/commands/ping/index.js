e410f1e0a446fb6a3cc9c699454bb9da3ec4699f
Mango <evan940415@gmail.com>
2023-06-22 08:54:13 +0800

    remake: all

--- src/commands/ping/index.js
+++
@@ -1,8 +0,0 @@
-import { SlashCommandBuilder } from 'discord.js'
-
-export const command = new SlashCommandBuilder().setName('ping').setDescription('Get discord bot latency')
-
-export const action = async (ctx) => {
-  ctx.reply('pong!')
-}
-
--- src/store/app.js
+++
@@ -1,7 +0,0 @@
-import { defineStore } from 'pinia'
-
-export const useCounterStore = defineStore('app', {
-  state: () => ({}),
-  getters: {},
-  actions: {},
-})
